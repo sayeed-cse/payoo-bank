774d4bfa2271650a888a9a0d77cddd21c99e8844
sayeed-cse <sayeed.web.dev@gmail.com>
2025-03-10 23:05:36 +0600

    login to main page link updated4.

--- script/login.js
+++ script/login.js
@@ -8,7 +8,8 @@ loginBtn.addEventListener('click', function (event) {
 
     if (accountNumber.length === 11) {
         if (convertedPin === 1234) {
-            window.location.href = "app://main.html";
+            // window.location.href = "main.html";
+            window.location.href = window.location.origin + "/main.html";
         }
         else {
             alert('pin not valid')
